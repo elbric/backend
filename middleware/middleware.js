4526b75f878d3d08a1a2f54c62c35165ecbafedc
forgetzz <forget.noxa90@gmail.com>
2026-05-29 08:09:52 +0800

    new

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,7 +1,12 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is missing")
+}
 
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
